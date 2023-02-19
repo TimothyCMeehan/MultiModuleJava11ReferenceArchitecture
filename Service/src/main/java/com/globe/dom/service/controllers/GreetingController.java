b86cf8e667244e8ff486a3fd6c958221059db0e5
Tim Meehan <tcmeehan@gmail.com>
2023-02-18 19:34:29 -0500

    Log4j2 and Lombok added

--- Service/src/main/java/com/globe/dom/service/controllers/GreetingController.java
+++ Service/src/main/java/com/globe/dom/service/controllers/GreetingController.java
@@ -1,25 +1,28 @@
 package com.globe.dom.service.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class GreetingController {
-
-    Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @RequestMapping("/")
     public String index() {
-        logger.trace("A TRACE Message");
-        logger.debug("A DEBUG Message");
-        logger.info("An INFO Message");
-        logger.warn("A WARN Message");
-        logger.error("An ERROR Message");
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
 
-        return System.getProperty("java.class.path");
-        //return "Howdy! Check out the Logs to see the output...";
+        return greeting("Howdy! Check out the Logs to see the output...");
+    }
+
+    public String greeting(String message){
+        return new ResponseEntity<>(message, HttpStatus.ACCEPTED).toString();
     }
 
 
