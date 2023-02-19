245ed361d80312e94b280fecfeec8b570dd749a1
Tim Meehan <tcmeehan@gmail.com>
2023-02-19 09:21:48 -0500

    Move swagger config to annotation

--- Service/src/main/java/com/globe/dom/service/ServiceApplication.java
+++ Service/src/main/java/com/globe/dom/service/ServiceApplication.java
@@ -1,10 +1,13 @@
 package com.globe.dom.service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Globedom.com API Docs", version = "v1.0.0", description = "GLOBE DOM REST API documentation"))
 public class ServiceApplication {
 
     public static void main(String[] args) {
